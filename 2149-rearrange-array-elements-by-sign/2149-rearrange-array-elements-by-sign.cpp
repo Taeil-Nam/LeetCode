88adac1771d0bf3b47af0d4a4cde1989df56653f
Taeil Nam (Tayden) <nam0314@gmail.com>
2024-02-14 12:42:22 +0900

    Time: 121 ms (98.17%), Space: 126.2 MB (60.27%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,52 +3,21 @@ class Solution
 public:
     vector<int> rearrangeArray(vector<int>& nums)
     {
-        unordered_map<int, bool> indexHash;
-        vector<int> result;
-        bool wasPositive = false;
-
-        // for (int i = 0; i < nums.size(); i++)
-        // {
-        //     if (nums[i] > 0 && wasPositive == false && indexHash[i] == false)
-        //     {
-        //         result.push_back(nums[i]);
-        //         wasPositive = true;
-        //         indexHash[i] = true;
-        //         i = -1;
-        //     }
-        //     else if (nums[i] < 0 && wasPositive == true && indexHash[i] == false)
-        //     {
-        //         result.push_back(nums[i]);
-        //         wasPositive = false;
-        //         indexHash[i] = true;
-        //         i = -1;
-        //     }
-        // }
-        // O(n^2) -> Time limits exceeded
-
+        vector<int> result(nums.size(), 0);
         int positiveIndex = 0;
-        int negativeIndex = 0;
-        while (positiveIndex < nums.size() && negativeIndex < nums.size())
+        int negativeIndex = 1;
+
+        for (int num : nums) // O(n)
         {
-            while (nums[positiveIndex] < 0 && positiveIndex < nums.size()) // O(n)
+            if (num > 0)
             {
-                positiveIndex++;
+                result[positiveIndex] = num;
+                positiveIndex += 2;
             }
-            while (nums[negativeIndex] > 0 && negativeIndex < nums.size()) // O(n)
+            else
             {
-                negativeIndex++;
-            }
-            if (positiveIndex < nums.size() && wasPositive == false)
-            {
-                result.push_back(nums[positiveIndex]);
-                positiveIndex++;
-                wasPositive = true;
-            }
-            if (negativeIndex < nums.size() && wasPositive == true)
-            {
-                result.push_back(nums[negativeIndex]);
-                negativeIndex++;
-                wasPositive = false;
+                result[negativeIndex] = num;
+                negativeIndex += 2;
             }
         }
         return result;
