83f247e6b134e26f160696e9385f4f0b50a022f8
Taeil Nam (Tayden) <nam0314@gmail.com>
2024-02-21 18:08:22 +0900

    Time: 6 ms (52.07%), Space: 15.5 MB (32.16%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int maxProfit(vector<int>& prices)
+    {
+        int profit = 0;
+        for (int i = 1; i < prices.size(); i++)
+        {
+            if (prices[i] - prices[i - 1] > 0)
+            {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+};
+
+// Time complexity = O(n)
+// Space complexity = O(1)
