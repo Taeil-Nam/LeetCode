b03d6605e11b2cb40556722e7e7a8572973b377f
Taeil Nam (Tayden) <nam0314@gmail.com>
2024-02-28 13:27:12 +0900

    Time: 0 ms (100%), Space: 7.7 MB (45.59%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,33 +1,48 @@
-// vector<string> split(string str, string delimiter)
-// {
-// 	vector<string> result;
-// 	size_t start = 0;
-// 	size_t end = str.find(delimiter);
-// 	while (end == string::npos)
-// 	{
-// 		result.push_back(str.substr(start, end - start));
-// 		start = end + delimiter.length();
-// 		end = str.find(delimiter, start);
-// 	}
-// 	result.push_back(str.substr(start));
-// 	return result;
-// }
-
 class Solution
 {
 public:
-    bool wordPattern(string pattern, string str)
+    bool wordPattern(string pattern, string s)
     {
-        map<char, int> p2i;
-        map<string, int> w2i;
-        istringstream in(str);
-        int i = 0, n = pattern.size();
-        for (string word; in >> word; ++i)
+        stringstream ss(s);
+        string word;
+        int index = 0;
+        int patternSize = pattern.size();
+        unordered_map<char, string> seen;
+        unordered_set<string> record;
+        while (getline(ss, word, ' ') && index <= patternSize) // O(n)
         {
-            if (i == n || p2i[pattern[i]] != w2i[word])
+            // 이미 있는 패턴인데, 기존 패턴에 대한 word랑 현재 word랑 다른 경우
+            if (seen.find(pattern[index]) != seen.end() && seen[pattern[index]] != word)
+            {
                 return false;
-            p2i[pattern[i]] = w2i[word] = i + 1;
+            }
+            // 처음 보는 패턴인데, 해당 패턴에 대한 word가 이전에 다른 패턴에 쓰인 경우
+            else if (seen.find(pattern[index]) == seen.end() && record.count(word) > 0)
+            {
+                return false;
+            }
+            seen[pattern[index]] = word; // 현재 패턴에 대한 word 저장
+            record.insert(word); // word가 사용되었다고 기록
+            index++;
         }
-        return i == n;
+        return index == patternSize; // 패턴 개수 만큼 word가 있었으면 true 반환
     }
 };
+
+// Time complexity = O(n)
+// Space complexity = O(n)
+
+/*
+Approch
+- ss : getline()을 사용하기 위한 stringstream. s의 스트림
+- seen : 특정 패턴에 대한 word가 무엇인지 매핑해놓는 map
+- record : 사용된 word들을 기록해놓는 set
+- index : pattern의 인덱스
+
+- getline()으로 ss에서 word들을 하나씩 가져오면서 반복문 진행
+    - 이미 있는 패턴인데, 기존 패턴에 대한 word랑 현재 word랑 다른 경우 false 반환
+    - 없는 패턴인데, 다른 패턴에 동일한 word가 쓰인 경우 false 반환
+    - seen에 현재 패턴에 대한 word 등록 (이미 있는 패턴이어도, word가 동일해서 갱신해도 상관 없음)
+    - record에 현재 word 기록
+- index와 patternSize가 같으면 true, 다르면 word가 부족한 경우이므로 false 반환
+*/
