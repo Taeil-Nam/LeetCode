c90a987639fd8c5931061305d5cd8ebaef277c6e
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-01-28 09:42:39 +0900

    Time: 4 ms (78.63%), Space: 93.9 MB (94.22%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,27 +1,28 @@
-int visited[15][15];
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-int cnt, res;
-
-void dfs(int y, int x, vector<vector<int>>& grid, int R, int C){
-    visited[y][x] = 1;
-    cnt += grid[y][x];
-    res = max(res, cnt);
-    for (int i = 0; i < 4; i++){
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
-        if (visited[ny][nx] || grid[ny][nx] == 0) continue;
-        dfs(ny, nx, grid, R, C);
-    }
-}
-
 class Solution {
 public:
+    int visited[15][15];
+    int dy[4] = {-1, 0, 1, 0};
+    int dx[4] = {0, 1, 0, -1};
+    int cnt = 0;
+    int res = 0;
+
+public:
+    void dfs(int y, int x, vector<vector<int>>& grid, int R, int C){
+        visited[y][x] = 1;
+        cnt += grid[y][x];
+        res = max(res, cnt);
+        for (int i = 0; i < 4; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny < 0 || ny >= R || nx < 0 || nx >= C) continue;
+            if (visited[ny][nx] || grid[ny][nx] == 0) continue;
+            dfs(ny, nx, grid, R, C);
+        }
+    }
+
     int findMaxFish(vector<vector<int>>& grid) {
         int R = grid.size();
         int C = grid[0].size();
-        res = 0;
         memset(visited, 0, sizeof(visited));
         for (int y = 0; y < R; y++){
             for (int x = 0; x < C; x++){
