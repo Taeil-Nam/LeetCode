a134ccc0c0c4f406a13bb3ba50dd4bea29ad2e4a
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-01-29 11:56:21 +0900

    Time: 70 ms (10.6%), Space: 34.3 MB (9.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,22 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs)
-    {
-        unordered_map<string, vector<string> > m;
-        for (int i = 0; i < strs.size(); i++) // O(n)
-        {
-            string key = getKey(strs[i]);
-            m[key].push_back(strs[i]);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+        for (string s : strs){
+            vector<int> cnt(26, 0);
+            for (char c : s){
+                cnt[c - 'a']++;
+            }
+            string key = "";
+            for (int n : cnt){
+                key += "," + to_string(n);
+            }
+            groups[key].push_back(s);
         }
-        vector<vector<string> > result;
-        for (auto value : m) // O(n)
-        {
-            result.push_back(value.second);
+        vector<vector<string>> res;
+        for (const auto& pair : groups){
+            res.push_back(pair.second);
         }
-        return result;
-    }
-
-private:
-    string getKey(string str)
-    {
-        string key;
-        vector<int> count(26, 0);
-        for (char c : str) // O(n)
-        {
-            count[c - 'a']++;
-        }
-        for (int i = 0; i < count.size(); i++) // O(1)
-        {
-            key.append(to_string(count[i]) + "#");
-        }
-        return key;
+        return res;
     }
 };
-
-// Time complexity = O(n^2)
-// Space complexity = O(n^2)
