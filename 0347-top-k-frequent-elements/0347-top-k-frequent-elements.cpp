ce992d72fe5e5b32b8209097bf8212e107908e60
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-01-30 11:53:51 +0900

    Time: 5 ms (28.51%), Space: 20 MB (6.56%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         vector<int> res;
-        unordered_map<int, int> um;
+        unordered_map<int, int> cnt;
         vector<vector<int>> freq(nums.size() + 1);
-        for (auto n : nums)
-            um[n]++;
-        for (auto [a, b] : um)
-            freq[b].push_back(a);
-        for (int i = nums.size(); i > 0; i--){
-            for (auto n : freq[i]){
-                if (res.size() == k)
+        for (int n : nums)
+            cnt[n]++;
+        for (auto p : cnt)
+            freq[p.second].push_back(p.first);
+        for (int i = nums.size(); i >= 0; i--){
+            for (int num : freq[i]){
+                res.push_back(num);
+                if (res.size() >= k)
                     return res;
-                res.push_back(n);
             }
         }
+        
         return res;
     }
 };
 /*
-[1] : 3
-[2] : 2
-[3] : 1
+1 : 3
+2 : 2
+3 : 1
 */
