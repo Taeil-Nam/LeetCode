634906ab5e96f641b7bc1ff13c8e7f4c43962051
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-02 18:40:10 +0900

    Time: 2 ms (45.4%), Space: 42.1 MB (32.52%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,38 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums)
-    {
-        vector<int> answer(nums.size(), 1);
-        int curr = 1;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            answer[i] *= curr; // 1 1 2 6
-            curr *= nums[i]; // 1 2 6 24
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int N = nums.size();
+        vector<int> res(N);
+        vector<int> prefix(N);
+        vector<int> suffix(N);
+        prefix[0] = 1;
+        suffix[N - 1] = 1;
+        for (int i = 1; i < N; i++){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
         }
-        curr = 1;
-        for (int i = nums.size() - 1; i >= 0; i--)
-        {
-            answer[i] *= curr; // 24 12 8 6
-            curr *= nums[i]; // 4 12 24 24
+        for (int i = N - 2; i >= 0; i--){
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         }
-        return answer;
+        for (int i = 0; i < N; i++){
+            res[i] = prefix[i] * suffix[i];
+        }
+        return res;
     }
 };
-
 /*
-[Variables]
-- vector<int> answer : i 번째 요소들 제외한 나머지 요소들의 곱셈 결과를 저장.
-- int curr : 현재까지의 누적곱 저장.
-
-[Logic]
-- answer의 요소들을 1로 초기화한다.
-- curr를 1로 초기화한다.
-- i를 제외한 왼쪽 부분의 곱을 구한다. (첫 번째 for loop)
-- i를 제외한 오른쪽 부분의 곱을 구하고, 미리 구해뒀던 왼쪽 부분의 곱과 곱한다.(두 번째 for loop)
-- answer를 반환한다.
-
-[Comoplexity]
-- Time complexity = O(n)
-- Space complexity = O(1) : answer는 제외.
+1  1  2 8 prefix
+1  2  4 6 nums
+48 24 6 1 suffix
 */
