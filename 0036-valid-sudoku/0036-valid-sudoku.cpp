a4e71dc6879611653e11ba3822fec51d74c41f8d
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-03 23:27:30 +0900

    Time: 11 ms (16.84%), Space: 27.6 MB (6.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_map<int, unordered_set<int>> rowMap, colMap;
-        map<pair<int, int>, unordered_set<int>> squareMap;
+        unordered_map<string, unordered_set<int>> squareMap;
         for (int row = 0; row < 9; row++){
             for (int col = 0; col < 9; col++){
-                pair<int, int> squareKey = {row / 3, col / 3};
+                string squareKey = "" + to_string(row / 3) + to_string(col / 3);
                 char val = board[row][col];
                 if (val == '.')
                     continue;
