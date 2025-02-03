bc415f8efb262f59e7f4493f2d8afa45051b083e
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-03 23:19:52 +0900

    Time: 11 ms (16.84%), Space: 26.3 MB (6.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,42 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int cnt[10];
+        unordered_map<int, unordered_set<int>> rowMap, colMap;
+        map<pair<int, int>, unordered_set<int>> squareMap;
         for (int row = 0; row < 9; row++){
-            memset(cnt, 0, sizeof(cnt));
             for (int col = 0; col < 9; col++){
-                int val = board[row][col];
+                pair<int, int> squareKey = {row / 3, col / 3};
+                char val = board[row][col];
                 if (val == '.')
                     continue;
-                cnt[val - '0']++;
-                if (cnt[val - '0'] >= 2)
+                if (rowMap[row].count(val) ||
+                    colMap[col].count(val) ||
+                    squareMap[squareKey].count(val))
                     return false;
-            }
-        }
-        for (int col = 0; col < 9; col++){
-            memset(cnt, 0, sizeof(cnt));
-            for (int row = 0; row < 9; row++){
-                int val = board[row][col];
-                if (val == '.')
-                    continue;
-                cnt[val - '0']++;
-                if (cnt[val - '0'] >= 2)
-                    return false;
-            }
-        }
-        for (int row = 0; row < 9; row += 3){
-            for (int col = 0; col < 9; col += 3){
-                memset(cnt, 0, sizeof(cnt));
-                for (int i = 0; i < 3; i++){
-                    for (int j = 0; j < 3; j++){
-                        int val = board[row + i][col + j];
-                        if (val == '.')
-                            continue;
-                        cnt[val - '0']++;
-                        if (cnt[val - '0'] >= 2)
-                            return false;
-                    }
-                }
+                rowMap[row].insert(val);
+                colMap[col].insert(val);
+                squareMap[squareKey].insert(val);
             }
         }
         return true;
