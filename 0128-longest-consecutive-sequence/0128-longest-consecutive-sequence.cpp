0193a1c32affe59257dc2d651e97fa7432e99536
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-04 10:25:48 +0900

    Time: 131 ms (24.43%), Space: 90.2 MB (7%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
         set<int> numsSet;
         for (int num : nums){
             numsSet.insert(num);
         }
         int beforeNum = INT_MIN;
         int cnt = 1;
-        int res = 0;
+        int res = 1;
         for (int num : numsSet){
             if (num == beforeNum + 1){
                 cnt++;
