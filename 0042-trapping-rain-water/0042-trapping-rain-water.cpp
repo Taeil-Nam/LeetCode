5cc75abb3991cf1b26bc62784d5cb6bcb1da09b6
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-09 10:53:41 +0900

    Time: 0 ms (100%), Space: 26.1 MB (62.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,67 +1,23 @@
-class Solution
-{
+class Solution {
 public:
-    int trap(vector<int>& height)
-    {
-        int lastMaxHeight = 0;
-        for (int i = 0; i < height.size(); i++) // O(n)
-        {
-            if (height[lastMaxHeight] <= height[i])
-            {
-                lastMaxHeight = i;
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int leftWall = height[left];
+        int rightWall = height[right];
+        int res = 0;
+        while (left < right){
+            if (leftWall < rightWall){
+                left++;
+                leftWall = max(leftWall, height[left]);
+                res += leftWall - height[left];
+            }
+            else{
+                right--;
+                rightWall = max(rightWall, height[right]);
+                res += rightWall - height[right];
             }
         }
-
-        int water = 0;
-        int leftWall = 0;
-        for (int i = 0; i < lastMaxHeight; i++) // O(l) : l = lastMaxHeight
-        {
-            if (height[leftWall] <= height[i])
-            {
-                leftWall = i;
-            }
-            else
-            {
-                water += height[leftWall] - height[i];
-            }
-        }
-
-        int rightWall = height.size() - 1;
-        for (int i = height.size() - 1; i >= lastMaxHeight; i--) // O(n - l) : l = lastMaxHeight
-        {
-            if (height[rightWall] <= height[i])
-            {
-                rightWall = i;
-            }
-            else
-            {
-                water += height[rightWall] - height[i];
-            }
-        }
-        
-        return water;
+        return res;
     }
 };
-
-/*
-Approch
-[Variables]
-- int lastMaxHeight : 가장 높은 곳이 마지막으로 위치하는 인덱스.
-- int water : 고인 물의 양을 저장.
-- int leftWall : 왼쪽 벽.
-- int rightWall : 오른쪽 벽.
-
-[Logic]
-- height를 순회하면서 가장 높은 곳이 마지막으로 위치하는 인덱스를 찾고 lastMaxHeight에 저장한다.
-- 0부터 lastMaxHeight까지 height를 순회하면서 아래 과정을 반복한다.
-    - 현재 height가 leftWall의 height보다 크거나 같으면, 해당 인덱스를 leftWall로 설정한다.
-    - 현재 height가 leftWall의 height보다 작으면, height[leftWall] - height[i] 값을 water에 더한다.
-- height의 size - 1부터 lastMaxHeight까지 height를 순회하면서 아래 과정을 반복한다.
-    - 현재 height가 leftWall의 height보다 크거나 같으면, 해당 인덱스를 leftWall로 설정한다.
-    - 현재 height가 leftWall의 height보다 작으면, height[leftWall] - height[i] 값을 water에 더한다.
-- water를 반환한다.
-
-[Complexity]
-- Time complexity = O(n)
-- Space complexity = O(1)
-*/
