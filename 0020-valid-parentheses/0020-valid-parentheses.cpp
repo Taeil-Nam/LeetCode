44c83c9469a41ad5ff7d4682458a7d6b1cf1f143
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-09 14:45:03 +0900

    Time: 0 ms (100%), Space: 8.8 MB (34.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,49 +1,20 @@
-#include <stack>
-
-class Solution
-{
+class Solution {
 public:
-    bool isValid(string s)
-    {
+    bool isValid(string s) {
         stack<char> st;
-        for (char c : s)
-        {
-            if (c == '(' || c == '{' || c == '[')
-            {
-                st.push(c);
+        for (char c : s){
+            if (c == ')' || c == '}' || c == ']'){
+                if (st.empty() ||
+                    c == ')' && st.top() != '(' ||
+                    c == '}' && st.top() != '{' ||
+                    c == ']' && st.top() != '[')
+                    return false;
+                else
+                    st.pop();
             }
             else
-            {
-                if (st.empty() ||
-                st.top() == '(' && c != ')' ||
-                st.top() == '{' && c != '}' ||
-                st.top() == '[' && c != ']')
-                {
-                    return false;
-                }
-                else
-                {
-                    st.pop();
-                }
-            }
+                st.push(c);
         }
         return st.empty();
     }
 };
-
-// Time complexity = O(n)
-// Space complexity = O(n)
-
-/*
-Approch
-
-- 괄호들이 들어 있는 문자열을 하나씩 검사한다.
-- 열린 괄호일 때
-    - 스택에 푸쉬한다.
-- 닫힌 괄호일 때
-    - 스택이 비어있거나, 스택의 맨 위의 요소(top)가 짝이 맞는 열린 괄호가 아닌 경우 false를 반환한다.
-    - 스택의 맨 위의 요소(top)가 짝이 맞는 열린 괄호일 경우 해당 요소를 스택에서 제거(pop)한다.
-- 반복문 종료 후
-    - 스택이 비어있는 경우, 짝이 맞는 괄호들이 들어왔음을 의미하므로 true를 반환한다.
-    - 스택이 비어있지 않은 경우, 짝이 없는 열린 괄호가 남아있음을 의미하므로 false를 반환한다.
-*/
