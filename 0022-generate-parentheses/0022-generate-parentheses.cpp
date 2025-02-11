a00119a5b2bd1cd71e963311ec464ec9d847d708
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-11 12:33:44 +0900

    Time: 3 ms (70.32%), Space: 15.8 MB (35.3%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        go("", n, res);
+        go("", 0, 0, n, res);
         return res;
     }
-    void go(string s, int n, vector<string>& res){
-        if (s.size() == n * 2){
-            int openCnt = 0;
-            for (char c : s){
-                if (c == '(')
-                    openCnt++;
-                else
-                    openCnt--;
-                if (openCnt < 0) return;
-            }
-            if (openCnt == 0) res.push_back(s);
+    void go(string s, int openCnt, int closeCnt, int n, vector<string>& res){
+        if (openCnt + closeCnt == 2 * n){
+            res.push_back(s);
             return;
         }
-        go(s + "(", n, res);
-        go(s + ")", n, res);
+        if (openCnt < n){
+            go(s + "(", openCnt + 1, closeCnt, n, res);
+        }
+        if (closeCnt < openCnt){
+            go(s + ")", openCnt, closeCnt + 1, n, res);
+        }
     }
 };
