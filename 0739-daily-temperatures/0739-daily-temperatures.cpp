5b66d3701ed23487945bc1afe5f0cf0b5a6724d6
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-12 12:26:40 +0900

    Time: 48 ms (5.36%), Space: 111.8 MB (16.42%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,30 +3,16 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         int N = temperatures.size();
         vector<int> res(N, 0);
-        stack<pair<int, int>> st; // <val, index>?
+        stack<pair<int, int>> st; // <val, index>
         for (int i = 0; i < temperatures.size(); i++){
             int val = temperatures[i];
-            if (st.size()){
-                int currentIndex = i;
-                while (st.size() && st.top().first < val){
-                    int beforeIndex = st.top().second;
-                    res[beforeIndex] = currentIndex - beforeIndex;
-                    st.pop();
-                }
+            while (st.size() && st.top().first < val){
+                int beforeIndex = st.top().second;
+                res[beforeIndex] = i - beforeIndex;
+                st.pop();
             }
             st.push({val, i});
-        }
-        while (st.size()){
-            res[st.top().second] = 0;
-            st.pop();
         }
         return res;
     }
 };
-/*
-idx          0  1  2  3  4  5  6
-tempers     [30,38,30,36,35,40,28]
-valStack    [40,28]
-idxStack    [5,6]
-res         [1,4,1,2,1,0,0]
-*/
