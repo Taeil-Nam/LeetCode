01cff464dc540c4f76415de2eedbb3cebe46f6f2
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-15 13:49:24 +0900

    Time: 0 ms (100%), Space: 19.5 MB (62.55%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,54 +8,28 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
-    {
-        ListNode* result = new ListNode();
-        ListNode* head = result;
-        while (list1 != nullptr && list2 != nullptr) // O(n)
-        {
-            if (list1->val <= list2->val)
-            {
-                result->next = list1;
-                result = result->next;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode dummy;
+        ListNode* node = &dummy;
+        while (list1 && list2){
+            if (list1->val <= list2->val){
+                node->next = list1;
                 list1 = list1->next;
             }
-            else
-            {
-                result->next = list2;
-                result = result->next;
+            else{
+                node->next = list2;
                 list2 = list2->next;
             }
+            node = node->next;
         }
-        while (list1 != nullptr) // O(n)
-        {
-            result->next = list1;
-            result = result->next;
-            list1 = list1->next;
+        if (list1){
+            node->next = list1;
         }
-        while (list2 != nullptr) // O(n)
-        {
-            result->next = list2;
-            result = result->next;
-            list2 = list2->next;
+        else{
+            node->next = list2;
         }
-        return head->next;
+        return dummy.next;
     }
 };
-
-/*
-[Approch]
-- 새로운 list의 dummy head를 만든다.
-- list1, list2를 처음부터 하나씩 비교하면서 아래 내용을 수행한다:
-    - 작은 값을 가진 노드 = 새로운 list->next로 설정한다.
-- list1을 전부 순회하면서 새로운 list에 추가한다.
-- list2를 전부 순회하면서 새로운 list에 추가한다.
-- 만들어진 리스트의 dummy head->next를 반환한다.
-
-[Complexity]
-- Time complexity = O(n)
-- Space complexity = O(n)
-*/
