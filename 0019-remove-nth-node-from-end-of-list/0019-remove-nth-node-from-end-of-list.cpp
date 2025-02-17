3f74bbac719fa527bd86a766d6d625ac1e550f89
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-17 10:59:43 +0900

    Time: 0 ms (100%), Space: 14.9 MB (91.48%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,49 +8,32 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution
-{
+
+class Solution {
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n)
-    {
-        ListNode* first = head;
-        ListNode* second = head;
-        for (int i = 0; i < n; i++) // O(n)
-        {
-            first = first->next;
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int size = 0;
+        ListNode* node = head;
+        while (node){
+            size++;
+            node = node->next;
         }
-        if (first == nullptr) // 첫번째 원소를 지우는 경우
-        {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-            return head;
+
+        ListNode dummy;
+        ListNode* prev = &dummy;
+        prev->next = head;
+        node = head;
+        int cnt = 0;
+        while (cnt < size - n){
+            prev = node;
+            node = node->next;
+            cnt++;
         }
-        while (first->next != nullptr) // O(n)
-        {
-            first = first->next;
-            second = second->next;
-        }
-        ListNode* temp = second->next;
-        second->next = second->next->next;
-        delete temp;
+        prev->next = node->next;
+        delete node;
+        if (size - n == 0)
+            head = prev->next;
+
         return head;
     }
 };
-
-/*
-Approch
-[Variables]
-- first = n만큼 먼저 출발하는 노드
-- second = n만큼 뒤떨어져서 출발하는 노드
-
-[Logic]
-- n 만큼 first를 이동시킨다.
-    - first가 nullptr인 경우, 맨 첫번째 노드를 삭제하는 것을 의미하므로, head를 head->next로 바꿔주고 head를 반환한다.
-- first->next가 nullptr을 만날 때까지 first와 second를 계속 이동시킨다.
-- second->next를 second->next->next로 바꾸고 head를 반환한다.
-
-[Complexity]
-- Time complexity = O(n)
-- Space complexity = O(1)
-*/
