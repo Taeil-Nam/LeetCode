3c126357db9fc2a476f632ba82f593ec0b02c0e8
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-02-22 20:35:45 +0900

    Time: 0 ms (100%), Space: 97.3 MB (60.73%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,27 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-    int maxProfit(vector<int>& prices)
-    {
-        int result = 0;
-        int minNumber = numeric_limits<int>::max();
-        int profit = 0;
-
-        for (int i = 0; i < prices.size(); i++)
-        {
-            if (prices[i] < minNumber)
-            {
-                minNumber = prices[i];
+    int maxProfit(vector<int>& prices) {
+        int left = 0;
+        int right = 1;
+        int res = 0;
+        while (right < prices.size()){
+            if (prices[left] < prices[right]){
+                res = max(res, prices[right] - prices[left]);
             }
-            profit = prices[i] - minNumber;
-            if (profit > result)
-            {
-                result = profit;
+            else{
+                left = right;
             }
+            right++;
         }
-        return result;
+        return res;
     }
 };
-
-// O(n^2) : time limit exceeded
-// 8 5 9 2 7
