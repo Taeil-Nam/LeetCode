6f3002b1448c104ade7781d707a7a7c161c9398d
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-10 10:40:13 +0900

    Time: 1 ms (64.65%), Space: 9.9 MB (25.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         unordered_map<char, int> um;
-        if (s.size() != t.size())
-            return false;
         for (char c : s)
             um[c]++;
-        for (char c : t){
+        for (char c : t)
             um[c]--;
-            if (um[c] < 0)
+        for (auto pair : um){
+            if (pair.second != 0)
                 return false;
         }
         return true;
