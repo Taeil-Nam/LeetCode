0aeeecbd91da66865a0914af155281b65e551436
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-10 10:53:24 +0900

    Time: 19 ms (50.13%), Space: 24.8 MB (85.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> groups;
-        for (string s : strs){
-            vector<int> cnt(26, 0);
-            for (char c : s){
-                cnt[c - 'a']++;
-            }
-            string key = "";
-            for (int n : cnt){
-                key += "," + to_string(n);
-            }
-            groups[key].push_back(s);
-        }
         vector<vector<string>> res;
-        for (const auto& pair : groups){
+        unordered_map<string, vector<string>> um;
+        for (string& str : strs){
+            string key = str;
+            sort(key.begin(), key.end());
+            um[key].push_back(str);
+        }
+        for (auto& pair : um){
             res.push_back(pair.second);
         }
         return res;
