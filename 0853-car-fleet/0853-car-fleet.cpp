7719a109ae4d5887520e27ca46ff969a93e8ddeb
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-11 14:24:10 +0900

    Time: 48 ms (53.01%), Space: 85.7 MB (51.16%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int N = position.size();
-        vector<pair<int, double>> v;
-        for (int i = 0; i < N; i++){
-            v.push_back({position[i], (double)(target - position[i]) / speed[i]});
+        vector<pair<int, int>> pairs;
+        for (int i = 0; i < position.size(); i++){
+            pairs.push_back({position[i], speed[i]});
         }
-        sort(v.rbegin(), v.rend());
-        double prevTime = 0;
-        int res = 0;
-        for (auto& [pos, time] : v){
-            if (prevTime < time){
-                prevTime = time;
+        sort(pairs.rbegin(), pairs.rend());
+
+        int res = 1;
+        double prevTime = (double)(target - pairs[0].first) / pairs[0].second;
+        for (int i = 1; i < pairs.size(); i++){
+            double currTime = (double)(target - pairs[i].first) / pairs[i].second;
+            if (currTime > prevTime){
                 res++;
+                prevTime = currTime;
             }
         }
         return res;
