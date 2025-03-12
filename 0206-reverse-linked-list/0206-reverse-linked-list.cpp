103562d5070aef1ae1aa48d989472f03f2684e80
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-12 11:41:59 +0900

    Time: 0 ms (100%), Space: 13.6 MB (9.81%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,18 +11,18 @@
 class Solution {
 public:
     /* Iteration */
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        while (head){
-            ListNode* temp = head->next;
-            head->next = prev;
-            prev = head;
-            head = temp;
-        }
-        return prev;
-    }
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode* prev = nullptr;
+    //     while (head){
+    //         ListNode* temp = head->next;
+    //         head->next = prev;
+    //         prev = head;
+    //         head = temp;
+    //     }
+    //     return prev;
+    // }
 
-    /* Recursion */
+    /* Recursion1 */
     // ListNode* reverseList(ListNode* head) {
     //     return go(nullptr, head);
     // }
@@ -35,4 +35,15 @@ public:
     //     node->next = prev;
     //     return head;
     // }
+
+    /* Recursion2 */
+    ListNode* reverseList(ListNode* head){
+        if (head == nullptr || head->next == nullptr){
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
 };
