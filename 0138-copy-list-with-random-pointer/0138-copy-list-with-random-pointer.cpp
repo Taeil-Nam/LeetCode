9c91d143ad4e9bd61eb78176df2a70295d03c501
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-12 14:45:05 +0900

    Time: 7 ms (53.27%), Space: 15.4 MB (10.23%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -17,18 +17,38 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> nodeMap;
-        Node* curr = head;
-        while (curr){
-            nodeMap[curr] = new Node(curr->val);
-            curr = curr->next;
+        Node dummy(0);
+        Node* newNode = &dummy;
+        Node* node = head;
+        while (node){
+            newNode->next = new Node(node->val);
+            newNode = newNode->next;
+            node = node->next;
         }
-        curr = head;
-        while (curr){
-            nodeMap[curr]->next = nodeMap[curr->next];
-            nodeMap[curr]->random = nodeMap[curr->random];
-            curr = curr->next;
+
+        vector<Node*> v;
+        unordered_map<Node*, int> um;
+        int i = 0;
+        node = head;
+        newNode = dummy.next;
+        while (node){
+            v.push_back(newNode);
+            um[node] = i;
+            i++;
+            newNode = newNode->next;
+            node = node->next;
         }
-        return nodeMap[head];
+
+        node = head;
+        newNode = dummy.next;
+        while (node){
+            if (node->random)
+                newNode->random = v[um[node->random]];
+            else
+                newNode->random = nullptr;
+            newNode = newNode->next;
+            node = node->next;
+        }
+        return dummy.next;
     }
 };
