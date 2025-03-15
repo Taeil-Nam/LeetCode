622c38a3ec527d938cc1f4b37aecfdc68f0f2878
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-15 18:14:57 +0900

    Time: 0 ms (100%), Space: 31.3 MB (45.57%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,15 +5,12 @@ public:
         int r = nums.size() - 1;
         while (l <= r){
             int m = (l + r) / 2;
-            if (target < nums[m]){
-                r = m - 1;
-            }
-            else if (target > nums[m]){
-                l = m + 1;
-            }
-            else{
+            if (nums[m] == target)
                 return m;
-            }
+            else if (target > nums[m])
+                l = m + 1;
+            else if (target < nums[m])
+                r = m - 1;
         }
         return -1;
     }
