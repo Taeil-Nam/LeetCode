8dac19aaf827c211870f8e304b4a9c5845a36946
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-15 19:01:00 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,7 +7,7 @@ public:
         while (l <= r){
             res = min(res, nums[l]);
             int m = (l + r) / 2;
-            if (nums[l] <= nums[m]){
+            if (nums[l] < nums[m] && nums[m] > nums[r]){
                 l = m + 1;
             }
             else
@@ -17,3 +17,9 @@ public:
         return res;
     }
 };
+
+// 1 2 3 4 5
+// 2 3 4 5 1
+// 3 4 5 1 2
+// 4 5 1 2 3
+// 5 1 2 3 4
