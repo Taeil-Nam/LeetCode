befa39e38ecb4595092e0437c364607579a9ee0f
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-15 18:19:51 +0900

    Time: 0 ms (100%), Space: 13.2 MB (76.23%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -7,15 +7,15 @@ public:
         int r = rowSize * colSize - 1;
         while (l <= r){
             int m = (l + r) / 2;
-            if (target == matrix[m / colSize][m % colSize]){
+            int row = m / colSize;
+            int col = m % colSize;
+            int val = matrix[row][col];
+            if (target == val)
                 return true;
-            }
-            else if (target < matrix[m / colSize][m % colSize]){
+            else if (target < val)
                 r = m - 1;
-            }
-            else if (target > matrix[m / colSize][m % colSize]){
+            else if (target > val)
                 l = m + 1;
-            }
         }
         return false;
     }
