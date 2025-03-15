738d377ef9858fa750b3706833195f11c3caf88b
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-15 18:43:48 +0900

    Time: 9 ms (54.27%), Space: 22.8 MB (96.68%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int l = 1;
-        int r = 1e9;
-        int res = r;
+        int r = 1e+9;
+        int res = INT_MAX;
         while (l <= r){
             int m = (l + r) / 2;
-            long long cnt = 0;
-            for (int pile : piles){
-                cnt += (pile + m - 1) / m;
+            long long time = 0;
+            for (auto pile : piles){
+                time += (pile + m - 1) / m;
             }
-            if (cnt <= h){
-                res = m;
-                r = m - 1;
-            }
-            else{
+            if (time > h)
                 l = m + 1;
+            else{
+                r = m - 1;
+                res = min(res, m);
             }
         }
         return res;
     }
 };
+
+/*
+1 2 3  4 5 6
+3 4 5  6 7 8
+1 1 1  2 2 2
+*/
