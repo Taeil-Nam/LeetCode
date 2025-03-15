d99bcdf450bbdcf7d07772184ce9d5f90452779f
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-15 17:56:46 +0900

    Time: 8 ms (44%), Space: 10.9 MB (43.53%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int left = 0, right = 0;
-        int maxFreq = 0;
-        int res = 0;
         unordered_map<char, int> freq;
-        while (right < s.size()){
-            freq[s[right]]++;
-            maxFreq = max(maxFreq, freq[s[right]]);
-            while ((right - left + 1) - maxFreq > k){
-                freq[s[left]]--;
-                left++;
+        int maxFreq = 0;
+        int l = 0;
+        int r = 0;
+        int res = 0;
+        while (r < s.size()){
+            freq[s[r]]++;
+            maxFreq = max(maxFreq, freq[s[r]]);
+            while ((r - l + 1) - maxFreq > k){
+                freq[s[l]]--;
+                l++;
             }
-            res = max(res, right - left + 1);
-            right++;
+            res = max(res, r - l + 1);
+            r++;
         }
         return res;
     }
