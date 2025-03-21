d9ddaf8f31d6327054fb5ea5a7ffe92da4834677
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-21 22:28:46 +0900

    Time: 0 ms (100%), Space: 19.2 MB (13.05%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -14,6 +14,21 @@ public:
     int maxDepth(TreeNode* root) {
         if (!root)
             return 0;
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+
+        queue<TreeNode*> q;
+        int depth = 0;
+
+        q.push(root);
+        while (q.size()){
+            depth++;
+            int levelSize = q.size();
+            for (int i = 0; i < levelSize; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+        return depth;
     }
 };
