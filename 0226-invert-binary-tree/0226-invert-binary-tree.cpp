a78c2fb76ba656fcf3a02bc3d14df79cc70bbe01
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-24 18:36:09 +0900

    Time: 0 ms (100%), Space: 12.5 MB (24.71%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,16 +12,11 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (!root) return nullptr;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (q.size()){
-            TreeNode* node = q.front();
-            q.pop();
-            swap(node->left, node->right);
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
-        }
+        if (!root)
+            return nullptr;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
         return root;
     }
 };
