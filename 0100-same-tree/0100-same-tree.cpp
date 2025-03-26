829c260ca8d800182cc792d054aa077d28c013dd
Taeil Nam (Tayden) <nam0314@gmail.com>
2025-03-26 19:42:33 +0900

    Time: 0 ms (100%), Space: 12.9 MB (47.36%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,27 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-class Solution
-{
+class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q)
-    {
-        if (p == nullptr && q == nullptr) { return true; }
-        if (p == nullptr || q == nullptr || p->val != q->val ) { return false; }
-        return (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q)
+            return true;
+        if (!p || !q)
+            return false;
+        
+        bool isSame = false;
+        if (p->val == q->val)
+            isSame = true;
+        return isSame && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
-
-// Time complexity = O(n)
-// Space complexity = O(n)
-
-/*
-Approch
-
-1. 두 노드가 nullptr인 경우, true를 반환한다. (동일하게 nullptr 이니깐)
-2. 두 노드 중 하나만 nullptr이거나(노드의 위치가 맞지 않음), 두 노드의 값이 다른 경우 false를 반환한다.
-3. 현재 노드의 left, right에 대해 1번 과정부터 재귀를 수행하고, 둘 다 true일 경우에만 true를 반환한다.
-
-최종적으로, 모든 조건이 true가 되었을 경우에만(모든 노드의 위치와 값이 같은 경우에만) true를 반환한다.
-*/
