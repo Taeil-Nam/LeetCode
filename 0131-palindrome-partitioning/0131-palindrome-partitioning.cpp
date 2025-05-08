906c0c9d9b10abdd19d54b0292bc8f43bc1e0663
Taeil Nam <nam0314@gmail.com>
2025-05-08 20:03:08 +0900

    Time: 38 ms (69.88%), Space: 53.5 MB (85.18%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
-        vector<string> combi;
+        vector<string> part;
 
-        backTrack(s, res, combi, 0);
+        backTrack(s, res, part, 0);
         return res;
     }
 
-    void backTrack(string& s, vector<vector<string>>& res, vector<string>& combi, int start){
+    void backTrack(string& s, vector<vector<string>>& res, vector<string>& part, int start){
         if (start == s.size()){
-            res.push_back(combi);
+            res.push_back(part);
             return;
         }
 
         for (int i = start; i < s.size(); i++){
             if (isPalindrome(s, start, i)){
-                combi.push_back(s.substr(start, i - start + 1));
-                backTrack(s, res, combi, i + 1);
-                combi.pop_back();
+                part.push_back(s.substr(start, i - start + 1));
+                backTrack(s, res, part, i + 1);
+                part.pop_back();
             }
         }
     }
 
-    bool isPalindrome(string& str, int i, int j){
-        while (i < j){
-            if (str[i] != str[j])
+    bool isPalindrome(string& s, int start, int i){
+        while (start < i){
+            if (s[start] != s[i])
                 return false;
-            i++;
-            j--;
+            start++;
+            i--;
         }
         return true;
     }
