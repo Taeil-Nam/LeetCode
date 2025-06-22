ddcafc5a726ebe70d14840a13d269080931e644d
Taeil Nam <nam0314@gmail.com>
2025-06-22 11:06:32 +0900

    Time: 14 ms (8.25%), Space: 25.9 MB (13.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_map<int, unordered_set<int>> rm;
-        unordered_map<int, unordered_set<int>> cm;
-        unordered_map<string, unordered_set<int>> sm;
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> cols(9);
+        vector<vector<unordered_set<char>>> boxes(3, vector<unordered_set<char>>(3));
+
         for (int row = 0; row < 9; row++){
             for (int col = 0; col < 9; col++){
-                int num = board[row][col];
-                if (num == '.')
-                    continue;
-                string smKey = "" + to_string(row / 3) + to_string(col / 3);
-                if (rm[row].count(num) || cm[col].count(num) || sm[smKey].count(num))
+                char val = board[row][col];
+                if (val == '.') continue;
+                if (rows[row].count(val) ||
+                    cols[col].count(val) ||
+                    boxes[row / 3][col / 3].count(val))
                     return false;
-                rm[row].insert(num);
-                cm[col].insert(num);
-                sm[smKey].insert(num);
+                rows[row].insert(val);
+                cols[col].insert(val);
+                boxes[row / 3][col / 3].insert(val);
             }
         }
         return true;
