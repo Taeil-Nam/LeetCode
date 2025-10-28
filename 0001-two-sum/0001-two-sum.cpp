6058c6a22c3fc55d4f8b3e6e8fe637def1a792a8
Taeil Nam <nam0314@gmail.com>
2025-10-28 21:10:22 +0900

    Time: 43 ms (27.14%), Space: 14.1 MB (64.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> freq;
-
         for (int i = 0; i < nums.size(); i++){
-            if (freq.count(target - nums[i]))
-                return {i, freq[target - nums[i]]};
-            freq[nums[i]] = i;
+            for (int j = i + 1; j < nums.size(); j++){
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
         }
         return {};
     }
