dcd0152d698b6ad36e53c83234fb25d2f2ee8a7b
Taeil Nam <nam0314@gmail.com>
2025-10-29 21:08:57 +0900

    Time: 2 ms (55.87%), Space: 9.9 MB (25.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<int, int> freq_s;
-        unordered_map<int, int> freq_t;
+        unordered_map<int, int> freq;
 
-        if (s.size() != t.size())
-            return false;
-        for (auto c : s){
-            freq_s[c]++;
+        for (int i = 0; i < s.size(); i++){
+            freq[s[i]]++;
         }
-        for (auto c : t){
-            freq_t[c]++;
+        for (int i = 0; i < t.size(); i++){
+            freq[t[i]]--;
         }
-        for (auto& it : freq_t){
-            if (freq_s[it.first] != it.second)
-                return false;
+        for (auto it : freq){
+            if (it.second != 0) return false;
         }
         return true;
     }
