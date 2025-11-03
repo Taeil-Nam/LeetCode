387ae2ac80a9876ceacdfbc5f386373933507f0f
Taeil Nam <nam0314@gmail.com>
2025-11-03 20:27:26 +0900

    Time: 11 ms (91.05%), Space: 24.9 MB (85.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        /*
-        * Data structure
-        - hashmap<string, vector<int>>
-
-        * Algorithm
-        1. Sort string and use this as a key of hashmap.
-        2. Add original string to vector in hashmap.
-        3. push_back each vector to result vector.
-        */
         unordered_map<string, vector<string>> hashMap;
         vector<vector<string>> res;
 
