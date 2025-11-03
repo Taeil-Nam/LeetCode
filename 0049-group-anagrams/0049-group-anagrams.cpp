5af91b382bb35bfc440794f483f7bf3b446f385f
Taeil Nam <nam0314@gmail.com>
2025-11-03 20:24:09 +0900

    Time: 12 ms (83.95%), Space: 24.9 MB (85.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> groups;
+        /*
+        * Data structure
+        - hashmap<string, vector<int>>
+
+        * Algorithm
+        1. Sort string and use this as a key of hashmap.
+        2. Add original string to vector in hashmap.
+        3. push_back each vector to result vector.
+        */
+        unordered_map<string, vector<string>> hashMap;
         vector<vector<string>> res;
 
         for (string str : strs){
-            vector<int> cnt(26, 0);
-            for (int c : str){
-                cnt[c - 'a']++;
-            }
-            string key = "";
-            for (int n : cnt){
-                key += n;
-            }
-            groups[key].push_back(str);
+            string key = str;
+            sort(key.begin(), key.end());
+            hashMap[key].push_back(str);
         }
-        for (auto& it : groups){
+        for (auto& it : hashMap){
             res.push_back(it.second);
         }
         return res;
