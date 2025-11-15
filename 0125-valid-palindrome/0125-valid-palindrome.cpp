efbc52e7d30939fb4fe91aa2946b8535d9fa797f
Taeil Nam <nam0314@gmail.com>
2025-11-15 18:55:58 +0900

    Time: 3 ms (33.35%), Space: 9.8 MB (94.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,17 +3,17 @@ public:
     bool isPalindrome(string s) {
         int l = 0;
         int r = s.size() - 1;
-        while (l < r){
-            if (!isalnum(s[l])){
+
+        while (l < r) {
+            if (isalnum(s[l]) == false) {
                 l++;
                 continue;
             }
-            else if (!isalnum(s[r])){
+            if (isalnum(s[r]) == false) {
                 r--;
                 continue;
             }
-            if (tolower(s[l]) != tolower(s[r]))
-                return false;
+            if (tolower(s[l]) != tolower(s[r])) return false;
             l++;
             r--;
         }
