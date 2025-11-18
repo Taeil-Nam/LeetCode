5678c784a87034a40dd78aee1dbe5c8e40047849
Taeil Nam <nam0314@gmail.com>
2025-11-18 20:08:31 +0900

    Time: 0 ms (100%), Space: 25.4 MB (24.44%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        bool rows[9][9] = {0};
-        bool cols[9][9] = {0};
-        bool boxes[9][9] = {0};
+        vector<unordered_set<char>> rows(9);
+        vector<unordered_set<char>> cols(9);
+        vector<unordered_set<char>> boxes(9);
 
-        for (int row = 0; row < 9; row++){
-            for (int col = 0; col < 9; col++){
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
                 char val = board[row][col];
                 if (val == '.') continue;
 
-                int boxIdx = (row / 3) * 3 + (col / 3);
-                val -= '1';
-                if (rows[row][val] || cols[col][val] || boxes[boxIdx][val])
+                int boxIdx = ((row / 3) * 3) + (col / 3);
+                if (rows[row].count(val) || cols[col].count(val) || boxes[boxIdx].count(val))
                     return false;
-                rows[row][val] = true;
-                cols[col][val] = true;
-                boxes[boxIdx][val] = true;
+                
+                rows[row].insert(val);
+                cols[col].insert(val);
+                boxes[boxIdx].insert(val);
             }
         }
         return true;
